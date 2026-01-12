7d728dc7bdfeb7d0cb0dc573e619e791e43285b4
zhirep <pelayoz@mymacewan.ca>
2026-01-12 15:09:02 -0700

    my first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+JavaScript
+window.onload = function() {
+    var button = document.querySelector('button');
+    button.addEventListener('click', function() {
+        alert("Let's Rock!!");
+    });
+}
